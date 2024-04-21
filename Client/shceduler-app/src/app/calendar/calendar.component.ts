11d642e9aecf1ad9d614d58ca7a090317c624aa2
Dmitry <sliser90@gmail.com>
2024-04-21 09:21:49 +0200

    add event

--- Client/shceduler-app/src/app/calendar/calendar.component.ts
+++ Client/shceduler-app/src/app/calendar/calendar.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
+import { Component, ViewChild } from '@angular/core';
+import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions, EventInput } from '@fullcalendar/core';
 import { CalendarService } from './calendar.service';
-import interationPlugin from '@fullcalendar/interaction'
+import interationPlugin, { DateClickArg } from '@fullcalendar/interaction'
 import timeGridWeek from '@fullcalendar/timegrid'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
@@ -13,7 +13,10 @@ import dayGridPlugin from '@fullcalendar/daygrid'
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
+
 export class CalendarComponent {
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+
   calendarOptions: CalendarOptions = {
     buttonText:{
       today: 'Сегодня',
@@ -24,6 +27,7 @@ export class CalendarComponent {
     },
     scrollTime: '08:00',
     locale: 'ru-RU',
+    timeZone: 'local',
     slotLabelFormat:{
       hour12: false ,
       hour: 'numeric',
@@ -43,15 +47,29 @@ export class CalendarComponent {
   }
   events: EventInput[]
 
-  constructor(private calendarService: CalendarService){}
+  constructor(private calendarService: CalendarService
+  ){}
 
   toggleWeekends() {
     this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
   }
 
-  handleDateClick(info: any) {
-    console.log(info);
+  handleDateClick(info: DateClickArg) {
+    var start = info.date;
+    const hoursToAdd = 1 * 60 * 60 * 1000;
+    var end = new Date(hoursToAdd + start.getTime())
+    var event: EventInput = {
+      start: start,
+      end: end,
+      title: 'New Event',
+      borderColor: 'red'
+    }
+    console.log(event);
+    var api = this.calendarComponent.getApi();
+    api.addEvent(event);
   }
+
+
 
   handleSelect(info: any){
     console.log(info)
